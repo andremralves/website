f8ddea1885cbdcb07ef3d9a1c724223a6f19e39c
Andre Alves <andremralves@gmail.com>
2022-10-20 17:11:17 -0300

    add: projects list with two projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import styles from '../styles/components/ProjectCard.module.css'
 
-type Props = {}
+type Props = {
+  img: string
+  title: string
+  tech: Array<string>
+  description: string
+  btnLink: string
+}
 
 const ProjectCard = (props: Props) => {
   return (
     <div className={styles.container}>
-      <img src="images/sunset.jpg" alt="" />
+      <div className={styles.img__container}>
+        <img src={props.img} alt={props.title} />
+      </div>
       <div className={styles.container__right}>
         <div>
-          <h1>Django CRM</h1>
-          <p>Django CRM</p>
+          <h1>{props.title}</h1>
+          <div className={styles.techRow}>
+            {props.tech.map((item) => (
+              <span className={styles.techTag}>{item}</span>
+            ))}
+          </div>
+          <p>{props.description}</p>
         </div>
-        <button>Learn More</button>
+        <div>
+          <button>
+            <a href={props.btnLink} target="_blank">
+              Learn More
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   )
--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -5,17 +5,25 @@ import ProjectCard from './ProjectCard'
 type Props = {}
 
 const ProjectsList = (props: Props) => {
-  function projects() {
-    let projects = []
-    for (let i = 0; i < 3; i++) {
-      projects.push(<ProjectCard />)
-    }
-    return projects
-  }
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>PROJECTS</h1>
-      <section id={styles.list}>{projects()}</section>
+      <section id={styles.list}>
+        <ProjectCard
+          img="https://user-images.githubusercontent.com/71379045/168422458-98dbd26d-f6be-4db5-b9ae-fb1d3279592c.png"
+          title="Django CRM"
+          tech={['Python', 'Django', 'Html', 'Css', 'Bootstrap']}
+          description="A CRM system to manage patients and appointments."
+          btnLink="https://github.com/andremralves/django-CRM"
+        />
+        <ProjectCard
+          img="https://user-images.githubusercontent.com/71379045/191885681-302e5da2-cf88-48b6-867c-8861122df90f.gif"
+          title="Sorting Visualizer"
+          tech={['C++', 'SFML']}
+          description="A desktop application that enables you to visualize how sorting algorithms work. It was built using the SFML framework"
+          btnLink="https://github.com/andremralves/cpp_sorting_visualizer"
+        />
+      </section>
     </div>
   )
 }
