2c157c097255729b9cd5ec94b6f875fe4c4613f8
Andre Alves <andremralves@gmail.com>
2022-10-26 02:47:58 -0300

    add title to Layout head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
+import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 import styles from '../styles/components/Layout.module.css'
 
 type Props = {
+  title: string
   children: JSX.Element
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ title, children }: Props) => {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <Header />
       <main className={styles.container}>{children}</main>
       <Footer />
     </>
   )
+}
+
+Layout.defaultProps = {
+  title: 'André Alves',
 }
 
 export default Layout
