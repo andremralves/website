e17b441b5b506b86cc22338733abdb224ca4ee04
Andre Alves <andremralves@gmail.com>
2022-11-28 01:27:12 -0300

    add key to TechList map

--- components/Skills/TechList.tsx
+++ components/Skills/TechList.tsx
@@ -9,7 +9,7 @@ const TechList = (props: TechListProps) => {
   return (
     <ul className="flex justify-center md:justify-start md:flex-col flex-wrap max-h-60">
       {props.items.map((item) => (
-        <li className="flex justify-center mb-2">
+        <li key={item.alt} className="flex justify-center mb-2">
           <SkillImage src={item.src} alt={item.alt} />
         </li>
       ))}
